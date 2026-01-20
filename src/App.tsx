827e26904aed2069bc32312ca1e7463cf861bc75
Louise Høpfner <lhoepfner@gmail.com>
2026-01-20 14:25:21 +0100

    minor color changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-7xl mt-32'>Pocket Spells</h1>
-        <h2 className='text-neutral-600 mt-8 text-2xl'>Every spell, right in your pocket</h2>
-        <p className='text-neutral-500 mt-2 text-base'> A DnD 5e spell tracker (coming soon)</p>
+        <h1 className='text-8xl mt-32'>Pocket Spells</h1>
+        <h2 className='text-stone-600 mt-8 text-2xl font-medium'>Every spell, right in your pocket</h2>
+        <p className='text-stone-600 mt-2 text-base'> A DnD 5e spell tracker (coming soon)</p>
       </div>
     </>
   )

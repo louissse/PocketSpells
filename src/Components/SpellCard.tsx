6526d85e7ba8e718787c9b45509597d362028900
Louise Høpfner <lhoepfner@gmail.com>
2026-02-03 11:07:50 +0100

    border on spell card

--- src/Components/SpellCard.tsx
+++ src/Components/SpellCard.tsx
@@ -2,8 +2,40 @@ import type { SpellDetail } from "../types/spell";
 import SchoolIcon from "./ui/SchoolIcon";
 
 export default function SpellCard(spell: SpellDetail) {
+  let borderClass = "border-slate-900/30";
+  switch (spell.school.index.toLowerCase()) {
+    case "abjuration":
+      borderClass = "border-cyan-600/70";
+      break;
+    case "conjuration":
+      borderClass = "border-violet-700/70";
+      break;
+    case "divination":
+      borderClass = "border-sky-700/70";
+      break;
+    case "enchantment":
+      borderClass = "border-fuchsia-700/50";
+      break;
+    case "evocation":
+      borderClass = "border-orange-500/50";
+      break;
+    case "illusion":
+      borderClass = "border-red-700/50";
+      break;
+    case "necromancy":
+      borderClass = "border-zinc-900/50";
+      break;
+    case "transmutation":
+      borderClass = "border-lime-600/70";
+  }
+
   return (
-    <div className="flex min-h-20 flex-col justify-between rounded border border-slate-900/30 bg-linear-to-b from-rose-50 to-slate-100 p-2 shadow-xs">
+    <div
+      className={
+        borderClass +
+        " flex min-h-20 flex-col justify-between rounded border bg-linear-to-b from-rose-50/30 to-pink-100/30 p-2 shadow-xs"
+      }
+    >
       <div className="flex items-center justify-between gap-2">
         <p className="flex items-center gap-1 font-semibold">
           <SchoolIcon size={18} school={spell.school.index} />
